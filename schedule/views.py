2ab8fcb868a70686cfadad9aed6e5c2d2ac68d5b
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-06 16:32:45 +0300

    Update views.py

--- schedule/views.py
+++ schedule/views.py
@@ -1,1 +1,7 @@
+from rest_framework import viewsets
+from .models import InjuryIncident
+from .serializers import InjuryIncidentSerializer
 
+class InjuryIncidentViewSet(viewsets.ModelViewSet):
+    queryset = InjuryIncident.objects.all()
+    serializer_class = InjuryIncidentSerializer
