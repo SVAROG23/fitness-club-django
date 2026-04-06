fae1ba02eec513ff40101350d8322548199bd0af
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-06 16:33:01 +0300

    Update serializers.py

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,1 +1,7 @@
+from rest_framework import serializers
+from .models import InjuryIncident
 
+class InjuryIncidentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InjuryIncident
+        fields = '__all__'
