5316a4bf0c43c5bd449108124be08ccfcae32533
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-06 16:50:55 +0300

    Update urls.py

--- fitness_club/urls.py
+++ fitness_club/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/clients/', include('clients.urls')),
