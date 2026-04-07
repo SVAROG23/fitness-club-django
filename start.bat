d0e87c9b874df789466fcc1074173cdd0916322d
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 12:17:01 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Fitness Club Django Server...
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+py manage.py makemigrations
+py manage.py migrate
+py manage.py runserver
 pause
