819f28f2afd392b7540a6941ad30214d2831c3e9
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:55:58 +0300

    Update urls.py

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InjuryIncidentViewSet
+from .views import BookingViewSet, GroupClassViewSet
 
 router = DefaultRouter()
-router.register(r'incidents', InjuryIncidentViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'group-classes', GroupClassViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
