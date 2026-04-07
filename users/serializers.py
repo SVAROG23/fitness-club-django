9af5ed32322feda4ec679927d078f2490e448994
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:50:54 +0300

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'phone']
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'role', 'phone', 'avatar')
+        read_only_fields = ('id',)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'role', 'phone')
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
