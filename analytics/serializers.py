17cb2f9df3be416f6c1d33e6385ae7b253034403
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:56:58 +0300

    Update serializers.py

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -1,1 +1,10 @@
+from rest_framework import serializers
+from .models import InjuryIncident
 
+class InjuryIncidentSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.user.get_full_name', read_only=True)
+    trainer_name = serializers.CharField(source='trainer.get_full_name', read_only=True)
+    
+    class Meta:
+        model = InjuryIncident
+        fields = '__all__'
