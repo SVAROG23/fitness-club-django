03ba745ed2a6a1263e8aa8521e4f55274129c683
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:53:10 +0300

    Update urls.py

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ClientProfileViewSet
+from .views import ClientProfileViewSet, AnthropometryViewSet
 
 router = DefaultRouter()
 router.register(r'profiles', ClientProfileViewSet)
+router.register(r'anthropometry', AnthropometryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
