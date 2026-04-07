41f97188fbd65ae4e0c1ed15a950dae3c9640c47
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:57:45 +0300

    Update apps.py

--- analytics/apps.py
+++ analytics/apps.py
@@ -1,6 +1,1 @@
-from django.apps import AppConfig
 
-class AnalyticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analytics'
-    verbose_name = 'Аналитика'
