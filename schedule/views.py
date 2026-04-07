39ccc380f91ce3f2099d82e876c44af164c330bf
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:55:45 +0300

    Update views.py

--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,22 @@
-from rest_framework import viewsets
-from .models import InjuryIncident
-from .serializers import InjuryIncidentSerializer
+from rest_framework import viewsets, permissions
+from django.db.models import Q
+from .models import Booking, GroupClass
+from .serializers import BookingSerializer, GroupClassSerializer
 
-class InjuryIncidentViewSet(viewsets.ModelViewSet):
-    queryset = InjuryIncident.objects.all()
-    serializer_class = InjuryIncidentSerializer
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'trainer':
+            return Booking.objects.filter(trainer=user)
+        elif user.role == 'client':
+            return Booking.objects.filter(client__user=user)
+        return Booking.objects.all()
+
+class GroupClassViewSet(viewsets.ModelViewSet):
+    queryset = GroupClass.objects.all()
+    serializer_class = GroupClassSerializer
+    permission_classes = [permissions.IsAuthenticated]
