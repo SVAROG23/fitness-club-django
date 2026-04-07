6415eb5f09e1cb04aa5a1a9834782954c30051eb
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 12:23:09 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Role(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    
-    def __str__(self):
-        return self.name
-
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Администратор'),
@@ -16,7 +10,6 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='client')
     phone = models.CharField(max_length=20, blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     
     def __str__(self):
         return f"{self.get_full_name()} ({self.get_role_display()})"
