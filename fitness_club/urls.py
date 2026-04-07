d7e0cdf319b03861cf54bcb8ae7d0e45ddb31f54
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:27:15 +0300

    Update urls.py

--- fitness_club/urls.py
+++ fitness_club/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/clients/', include('clients.urls')),
@@ -11,3 +11,6 @@ urlpatterns = [
     path('api/schedule/', include('schedule.urls')),
     path('api/analytics/', include('analytics.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
