1ed9158d403b1a11a09f5e00fad9ba6644f41a99
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:56:38 +0300

    Update models.py

--- analytics/models.py
+++ analytics/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from users.models import User
+from clients.models import ClientProfile
+from workouts.models import Workout
+
+class InjuryIncident(models.Model):
+    client = models.ForeignKey(ClientProfile, on_delete=models.CASCADE, related_name='injuries')
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reported_injuries')
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    reason = models.CharField(max_length=200)
+    action_taken = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"Травма: {self.client.user.get_full_name()} - {self.date.date()}"
