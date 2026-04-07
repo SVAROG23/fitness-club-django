0c8573d2c1d57ff4bec94762047e347dda95ee13
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 12:28:53 +0300

    Update views.py

--- schedule/views.py
+++ schedule/views.py
@@ -1,22 +1,21 @@
 from rest_framework import viewsets, permissions
-from django.db.models import Q
 from .models import Booking, GroupClass
 from .serializers import BookingSerializer, GroupClassSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
     
     def get_queryset(self):
         user = self.request.user
-        if user.role == 'trainer':
+        if user.is_authenticated and user.role == 'trainer':
             return Booking.objects.filter(trainer=user)
-        elif user.role == 'client':
+        elif user.is_authenticated and user.role == 'client':
             return Booking.objects.filter(client__user=user)
         return Booking.objects.all()
 
 class GroupClassViewSet(viewsets.ModelViewSet):
     queryset = GroupClass.objects.all()
     serializer_class = GroupClassSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
