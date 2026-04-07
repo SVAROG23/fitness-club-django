575cbd14e8c5d8be549145cc2f3f4e3823348d8d
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 12:10:35 +0300

    Update apps.py

--- workouts/apps.py
+++ workouts/apps.py
@@ -1,6 +1,1 @@
-from django.apps import AppConfig
 
-class WorkoutsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workouts'
-    verbose_name = 'Тренировки и упражнения'
