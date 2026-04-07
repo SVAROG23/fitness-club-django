946d3b7288a31fe4dcdfce31e5840f3fc44b0ee6
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 12:25:32 +0300

    Update views.py

--- clients/views.py
+++ clients/views.py
@@ -6,17 +6,17 @@ from .serializers import ClientProfileSerializer, AnthropometrySerializer
 class ClientProfileViewSet(viewsets.ModelViewSet):
     queryset = ClientProfile.objects.all()
     serializer_class = ClientProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
     
     def get_queryset(self):
         user = self.request.user
-        if user.role == 'trainer':
+        if user.is_authenticated and user.role == 'trainer':
             return ClientProfile.objects.filter(trainer=user)
-        elif user.role == 'client':
+        elif user.is_authenticated and user.role == 'client':
             return ClientProfile.objects.filter(user=user)
         return ClientProfile.objects.all()
 
 class AnthropometryViewSet(viewsets.ModelViewSet):
     queryset = Anthropometry.objects.all()
     serializer_class = AnthropometrySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
