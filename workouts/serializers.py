70ec84b8cb9beae1f2eceededa195cc01480a078
SVAROG23 <147057595+SVAROG23@users.noreply.github.com>
2026-04-07 11:54:12 +0300

    Update serializers.py

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -1,17 +1,43 @@
 from rest_framework import serializers
-from .models import WorkoutProgram, Workout, Exercise
+from .models import MuscleGroup, Exercise, WorkoutProgram, Workout, WorkoutExercise, SetResult
 
-class WorkoutProgramSerializer(serializers.ModelSerializer):
+class MuscleGroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = WorkoutProgram
+        model = MuscleGroup
+        fields = '__all__'
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    muscle_group_name = serializers.CharField(source='muscle_group.name', read_only=True)
+    
+    class Meta:
+        model = Exercise
+        fields = '__all__'
+
+class SetResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SetResult
+        fields = '__all__'
+
+class WorkoutExerciseSerializer(serializers.ModelSerializer):
+    exercise_name = serializers.CharField(source='exercise.name', read_only=True)
+    sets = SetResultSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = WorkoutExercise
         fields = '__all__'
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    exercises = WorkoutExerciseSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Workout
         fields = '__all__'
 
-class ExerciseSerializer(serializers.ModelSerializer):
+class WorkoutProgramSerializer(serializers.ModelSerializer):
+    workouts = WorkoutSerializer(many=True, read_only=True)
+    client_name = serializers.CharField(source='client.user.get_full_name', read_only=True)
+    author_name = serializers.CharField(source='author.get_full_name', read_only=True)
+    
     class Meta:
-        model = Exercise
+        model = WorkoutProgram
         fields = '__all__'
